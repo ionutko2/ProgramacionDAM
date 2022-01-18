4c2d4166c77c4c4af274b5c02970fcb8789a90c1
ionutko2 <aa21.ivtoma66@iespabloserrano.com>
2022-01-18 14:02:02 +0100

    EmpleadoMalHecho

---
+++ Tema5.1/src/TrabajadorPrueba.java
@@ -0,0 +1,11 @@
+
+public class TrabajadorPrueba {
+	public static void main (String [] args) {
+		Trabajador nuevoTrabajador = new Trabajador();
+			
+		nuevoTrabajador.getAñosEmpresa();
+		nuevoTrabajador.getEstadoCivil();
+		nuevoTrabajador.getNombre();
+		nuevoTrabajador.
+	}
+}
